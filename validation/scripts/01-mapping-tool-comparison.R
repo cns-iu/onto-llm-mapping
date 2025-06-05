11202f4aba4031d19cb1e10d40f08f8f513e283d
Michael Ginda <michael.ginda@gmail.com>
2025-06-05 14:08:54 -0400

    Review of validation script 01
    Review prior work, clean up script to stream line OBO and CNS SSSOM mapping comparison.
    Filters to keep only skos:exactMatch mappings from OBO. Filters out Geno Ontology mappings.
    Adds clarification notes to script.
    Adds mapping record count for subject concepts (MeSH).
    Cleans up field set exported by script.

--- validation/scripts/01-mapping-tool-comparison.R
+++ validation/scripts/01-mapping-tool-comparison.R
@@ -10,9 +10,6 @@ library(stringr)
 # Set Paths
 path_eval_data <- paste0("./validation/evaluation_mappings")
 path_obo_eval <- paste0("https://github.com/mapping-commons/mesh-mappings/raw/refs/heads/first/mappings/")
- 
-# # Identify mapping project from file path
-# mapping_project <- unlist(str_split(llm_mapping_paths[1], pattern="\\/"))[[3]]
 
 #### Load Data ####
 ##### SSSOM Mapping Evaluation Tools ####
@@ -28,13 +25,13 @@ if(file.exists(paste0(path_eval_data,"/obo_mesh.sssom.csv"))==F){
 
 ##### CNS Naive SSSOM Mappings ####
 evaluative_mappings <- 
-  read.csv(file=paste0(path_eval_data,"/mesh-uberon-human-mapping.validation-lookup-table.dev.csv"),
+  read.csv(file=paste0(path_eval_data,
+                       "/mesh-uberon-human-mapping.validation-tool-development.csv"),
            header = T, encoding = "UFT-8")
 
 # Compare OBO and CNS Naive Mappings
 # Note: OBO is based on SME ground truth, and will correct errors found in CNS 
-# Naive mapping results.
-
+# non-expert mapping results.
 # Create id for joining evaluating mapping results
 obo_mappings$object_id <- str_replace(obo_mappings$object_id,"\\:","\\_")
 obo_mappings$subject_id <- str_replace(obo_mappings$subject_id,"\\:","\\_")
@@ -44,6 +41,10 @@ obo_mappings$pair_id <- paste0(obo_mappings$object_uri,"|",obo_mappings$subject_
 obo_mappings$weight <- 1
 
 # Convert OBO object_id and subject_id to URI pattern in CNS mappings.
+# Note: Here the scripts inverts the OBO mapping subject and object labels to
+# align with the CNS mapping statements. However, the cardinality of OBO mapping
+# predicates have not be inverted (e.g., narrowMatch -> broadMatch, and vice versa).
+# This is not an issue for this analysis, because are work focuses on exact matches.
 obo_mappings <- 
   obo_mappings %>%
   select(pair_id,object_uri,object_label,predicate_id,
@@ -51,12 +52,20 @@ obo_mappings <-
          author_id,weight)
 names(obo_mappings)[c(2,3,5,6)] <- c("subject_id","subject_label",
                                      "object_id","object_label")
-
 write.csv(obo_mappings, file=paste0(path_eval_data,"/obo_mesh.sssom.prepared.csv"),
           row.names = F, fileEncoding = "UTF8")
 
 #### Create SSSOM mapping comparison set. ####
-mapping_comparison <- full_join(evaluative_mappings,obo_mappings, by="pair_id")
+#mapping_comparison <- full_join(evaluative_mappings,obo_mappings, by="pair_id")
+mapping_comparison <- 
+  full_join(evaluative_mappings,
+            obo_mappings[obo_mappings$predicate_id=="skos:exactMatch",],
+            by="pair_id")
+
+# Remove GO ontology mappings, which are not apart of Onto-LLM test data
+mapping_comparison<- 
+  mapping_comparison %>% 
+  filter(!grepl("/GO_", object_id.y))
 
 # Clean up common fields 
 mapping_comparison[is.na(mapping_comparison$rec_id),]$subject_id.x <-
@@ -74,27 +83,43 @@ mapping_comparison[is.na(mapping_comparison$rec_id),]$predicate_id.x <-
 mapping_comparison[is.na(mapping_comparison$confidence),]$confidence <- 0
 mapping_comparison[is.na(mapping_comparison$weight),]$weight <- 0
 
+# Copy OBO authors record and confidence to alternative variables
+mapping_comparison$author_2 <- mapping_comparison$author_id
+mapping_comparison$confidence_a_2 <- mapping_comparison$confidence
+
 # Re-order and re-name variables
 mapping_comparison <-
   mapping_comparison %>%
   select(rec_id,pair_id,subject_id.x,subject_label.x,predicate_id.x,
          object_id.x,object_label.x,mesh_concept_group,map_state,
-         exclusion_reason,mapping_count,accurate_mapping_r_1,weight,
-         confidence_r_1, confidence,reviewer_1,author_id) %>%
+         exclusion_reason,accurate_mapping_a_1,weight,
+         confidence_a_1, confidence_a_2,author_1,author_2) %>%
   rename_with(~str_remove(., '.x')) %>%
-  arrange(subject_id,object_id) %>%
+  arrange(subject_id, object_id) %>%
   mutate(rec_id = seq_along(subject_id))
 
-names(mapping_comparison)[c(13,15,16,17)] <- 
-  c("accurate_mapping_r_2","confidence_r_2","author_id_1","author_id_2")
+names(mapping_comparison)[c(10,12,15,16)] <- 
+  c("exclusion_reason","accurate_mapping_a_2","author_id_1","author_id_2")
 
 # Calculate difference in mapping scores
 mapping_comparison <-
   mapping_comparison %>%
-  mutate(mapping_diff = accurate_mapping_r_2-accurate_mapping_r_1)
-mapping_comparison <- mapping_comparison[,c(1:13,18,14:17)]
+  mutate(mapping_diff = accurate_mapping_a_2-accurate_mapping_a_1)
+mapping_comparison <- mapping_comparison[,c(1:12,17,13:16)]
 
+# Add variable for reviewer to indicate mapping record to use in validation
 mapping_comparison$use_rec <- 1
 
-write.csv(mapping_comparison, file=paste0(path_eval_data,"/mesh-uberon-human-mapping.evaluate.csv"),
+# Count number of records to aid reviewer
+record_counts <- 
+  mapping_comparison %>%
+  ddply(.(subject_id), summarise,
+        record_count = length(rec_id))
+
+mapping_comparison <- 
+  join(mapping_comparison, record_counts, by="subject_id")
+
+# Save mapping tool comparison data set for review.
+write.csv(mapping_comparison, 
+          file=paste0(path_eval_data,"/mesh-uberon-human-mapping.evaluate.csv"),
           row.names = F, fileEncoding = "UTF8")
